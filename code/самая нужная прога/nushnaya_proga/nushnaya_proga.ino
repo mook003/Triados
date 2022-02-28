5c72717882321e6c1002981f4ac26b62f11b11b9
qb00m <95272383+qb00m@users.noreply.github.com>
2022-02-28 23:34:07 +0700

    Add files via upload

---
+++ code/самая нужная прога/nushnaya_proga/nushnaya_proga.ino
@@ -0,0 +1,47 @@
+#include <iarduino_MultiServo.h>        
+iarduino_MultiServo MSS;
+#include "GParser.h"
+#include "GyverMotor.h" 
+GMotor motorR1(DRIVER2WIRE, 2, 3, HIGH); 
+GMotor motorL1(DRIVER2WIRE, 4, 5, HIGH);
+GMotor motorR2(DRIVER2WIRE, 7, 6, HIGH); 
+GMotor motorL2(DRIVER2WIRE, 8, 9, HIGH);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(5);
+  motorL1.setMode(AUTO); 
+  motorR1.setMode(AUTO);
+  motorL2.setMode(AUTO); 
+  motorR2.setMode(AUTO);
+  MSS.servoSet(0, SERVO_MG996R);  
+  MSS.servoSet(1, SERVO_MG996R);    
+  MSS.begin();
+}
+void loop() {
+  if (Serial.available()) {
+    char str[30];
+    int amount = Serial.readBytesUntil(';', str, 30);
+    str[amount] = NULL;
+    GParser data(str, ',');
+    int ints[5];
+    int am = data.parseInts(ints);
+  
+    switch (ints[0]) {  // ключ
+      case 0:
+        motorL1.setSpeed(ints[1]);
+        motorR1.setSpeed(ints[2]);  
+        motorL2.setSpeed(ints[3]);
+        motorR2.setSpeed(ints[4]); 
+        digitalWrite(LED_BUILTIN, HIGH);   
+        delay(200);                        
+        digitalWrite(LED_BUILTIN, LOW);   
+        delay(100);      
+        break;
+      case 1:
+        MSS.servoWrite(0, ints[1]);
+        MSS.servoWrite(1, ints[2]);
+        break;
+      }
+  }
+}
