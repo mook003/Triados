f279493f77c75745fb8dae92be33c580fcb6f562
Eugen <95272261+mook003@users.noreply.github.com>
2022-03-20 13:34:13 +0700

    Update hand_node.py

--- code/nodes/hand_node.py
+++ code/nodes/hand_node.py
@@ -3,15 +3,26 @@ import roslib; roslib.load_manifest('aiden')
 import rospy
 from aiden.srv import *
 
-booms = [0.1, 0.08, 0.04, 0.04]
+angle = [0,0,"false"]
 joint_angle = [0,0,0,0]
 def move(req):
-	print(req.a, req.b, req.c)
+	global angle
+	rospy.Subscriber("/zed2/zed_node/imu/data", Imu, callbackIMU)
+	angle[0] += 180
+	while angle[0]+1<angle[1] or angle[0]+1<angle[1]:
+		rospy.Subscriber("/zed2/zed_node/imu/data", Imu, callbackIMU)
+		ard.write(bytes("0,60,-60,60,-60"))
+	angle[2] ="false"
+	print("vse")
 	res = hand(req.a, req.b, req.c)
 	return MessagesResponse("nah")
 
-def hand(x, y, z):
-	
+def callbackIMU1 (data):
+	global angle
+	if angle[2]!="false":
+		angle[0] = data.orientation.z * 90/0.71
+		angle[2] = "true"
+	angle[1] = data.orientation.z * 90/0.71
 
 def main_com():
 	rospy.init_node("hand_node")
