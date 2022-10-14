96acdb75bdc7aa5bd199a269ec4b4dfba86adb9b
Eugen <95272261+mook003@users.noreply.github.com>
2022-10-14 22:31:11 +0700

    Update move_node.py

--- code/nodes/move_node.py
+++ code/nodes/move_node.py
@@ -1,55 +1,72 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('aiden')
+import sys
 import rospy
 import serial
-import math
 
+from aiden.srv import *
+from sensor_msgs.msg import Image
+from zed_interfaces.msg import Object
+from zed_interfaces.msg import ObjectsStamped
 from std_msgs.msg import String
-from sensor_msgs.msg import Imu
 
-rbt_coord = [0,0,0]
-rbt_angle = 0
-s = 0
-motion_coord = []
+obj_data = []
+#
+#def hand_com(x, y, z):
+#	rospy.wait_for_service("hand_srv")
+#	try: 
+#		move_server = rospy.ServiceProxy("hand_srv", Messages)
+#		data = move_server(x, y, z)
+#		return data.report
+#	except rospy.ServiceException as e:
+#		return e
 
+# Move node communication function
+#def move_com(x, y, z):
+#	global pub
+#	pub.publish(String("{} {} {}".format(x, y, z)))
+#	print("{} {} {}".format(x, y, z))
 
+# Received data processing function
 
-def callbackIMU (data):
-	global rbt_angle, motion_coord
-	rbt_angle = data.orientation.z * 90/0.71 + 100000
+# RIGHT_HANDED_Z_UP_X_FWD
+
+def go_to_object():
+	global obj_data
+	x, y, z = obj_data[2][0], obj_data[2][1], obj_data[2][2]
 	
+	y_coef = 0.25 * y ** 3
+	rospy.loginfo("* povorot *")
+	speed = y		
+
 def callback(data):
-	global math, motion_coord, rbt_angle 
-	coord = list(map(float, data.data.split()))
-	coord[0]-=0.0468
-	coord[1]+=0.0585
-	motion_coord = [math.atan(coord[1] / coord[0]) * 180 / math.pi, coord[0]]
-	print("motion_coord: {}".format(motion_coord))
-	if motion_coord[0] > 2:
-		ard.write(bytes("0,80,-80,80,-80"))
-	if motion_coord[0] < -2:
-		ard.write(bytes("0,-80,80,-80,80"))
-	if motion_coord[0] > -2 and motion_coord[0] < 2 and motion_coord[1] > 0.35:
-		ard.write(bytes("0,-80,-80,-80,-80"))
-	if motion_coord[1] <= 0.35:
-		ard.write(bytes("0,0,0,0,0"))
+	global obj_data
+	rospy.loginfo("***** New Object *****")
+	for i in data.objects:
+		rospy.loginfo("***** New Object Find*****")
+		rospy.loginfo(i.sublabel)
+		rospy.loginfo([i.position[0], i.position[1], i.position[2]])
+		if i.sublabel == "Apple":
+			obj_data = [i.sublabel, i.label_id, [i.position[0], i.position[1], i.position[2]], i.confidence, i.tracking_state ]
+			break
+
 def main():
-	rospy.init_node('move_node')
-	#rate = rospy.Rate(1)
-	rospy.Subscriber("main_node_move", String, callback)
+	rospy.loginfo("Start")
+	global obj_data
+	rospy.Subscriber("/zed2/zed_node/obj_det/objects", ObjectsStamped, callback)
+	print(obj_data)
+	if not obj_data:
+		print(obj_data)
+		
+	if obj_data:
+		go_to_object()
+
 	rospy.spin()
-
-
-
+	#!!!wheel_arduino.close()
 
 if __name__ == '__main__':
-	ard = serial.Serial('/dev/ttyACM0', baudrate = 115200)
+	#!!!wheel_arduino = serial.Serial('/dev/ttyACM0', baudrate = 115200)
+	rospy.loginfo("***** Start *****")
+	#pub = rospy.Publisher('main_node_move', String)
+	rospy.init_node('main_node')
 	main()
-
-#ard.write(bytes("0,50,-50,50,-50", "utf-8"))
-
-#print("rbt_angle: {}".format(rbt_angle))
-#print("data.orientation.z: {}".format(data.orientation.z))
-#print("data.orientation.z r: {}".format(data.orientation.z * 90/0.71))
-#print("data.orientation.z * 90/0.71 - rbt_angle: {}".format(data.orientation.z * 90/0.71 - rbt_angle))
-#rospy.Subscriber("/zed2/zed_node/imu/data", Imu, callbackIMU)
